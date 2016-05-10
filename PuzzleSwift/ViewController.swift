04010c2c2be0842bc104f2360b205a4fcd70867f
frcocoatst <friedrichhaeupl@friedrichs-macbook-pro.local>
2016-05-10 16:44:33 +0200

    Initial Commit

---
+++ PuzzleSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PuzzleSwift
+//
+//  Created by Friedrich HAEUPL on 10.05.16.
+//  Copyright © 2016 Friedrich HAEUPL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
