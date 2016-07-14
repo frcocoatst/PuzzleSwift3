9c04e703a73556a8092cce4a19a55d6366273b9a
frcocoatst <friedrichhaeupl@friedrichs-mbp.fritz.box>
2016-07-14 11:28:07 +0200

    first try

---
+++ PuzzleSwift/Element.swift
@@ -0,0 +1,14 @@
+//
+//  Element.swift
+//  PuzzleSwift
+//
+//  Created by Friedrich HAEUPL on 11.05.16.
+//  Copyright © 2016 Friedrich HAEUPL. All rights reserved.
+//
+
+import Cocoa
+
+struct Element {
+    var index:Int
+    var color:Int
+}
